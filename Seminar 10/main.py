0880c4c2a160f850a5dad7083b793317c3bfcacb
Senior.Remi <senior.remi@MacBook-Pro-SeniorRemi.local>
2023-10-13 12:39:55 +0300

    HomeWork

---
+++ Seminar 10/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+data['robot'] = data['whoAmI'].apply(lambda x: 1 if x == 'robot' else 0)
+data['human'] = data['whoAmI'].apply(lambda x: 1 if x == 'human' else 0)
+
+data = data.drop('whoAmI', axis=1)
+
+print(data.head())
